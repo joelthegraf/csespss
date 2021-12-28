c2bf935a27efb27338afc302dbe79e5b14a12a47
Joel Graf <graf.joel03@gmail.com>
2021-12-28 23:18:52 +0100

    feat: added c++ solution of missing_number.

---
+++ introductory_problems/missing_number/solution.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int n, p, m = 0;
+    std::cin >> n;
+    for (int i = 1; i <= n; i++) {
+        m ^= i;
+    }
+    for (int i = 0; i < (n - 1); i++) {
+        std::cin >> p;
+        m ^= p;
+    }
+    std::cout << m;
+}
