3d7475c6d3fe4c9b8dc3ee29da63c1551323c30d
Joel Graf <graf.joel03@gmail.com>
2021-12-29 13:03:26 +0100

    refactor: simplified c++ solution of increasing_array.

--- introductory_problems/increasing_array/solution.cpp
+++ introductory_problems/increasing_array/solution.cpp
@@ -10,10 +10,7 @@ int main()
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
-    }
-    for (int i = 1; i < n; i++)
-    {
-        if (a[i - 1] > a[i])
+        if (i > 0 && a[i - 1] > a[i])
         {
             long d = a[i - 1] - a[i];
             c += d;
